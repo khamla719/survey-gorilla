4094b22e18000d5313453178be2999971ebbaa14
Todd Seller <me@toddseller.com>
2016-03-06 15:08:38 -0800

    Tweaks to login

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -39,7 +39,7 @@ post '/users/login' do
   user = User.find_by(email: params[:email])
   if user.authenticate(params[:password])
     session[:user_id] = user.id
-    session[:name] = user.full_name
+    session[:name] = user.first_name
     redirect '/users'
   else
     erb :index
