fe9c68c4214634d4911fe4e4936b97e2f6ae49e1
Todd Seller <me@toddseller.com>
2016-03-06 14:52:44 -0800

    Add models and migrations

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,23 @@
 10.times {User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, user_name: Faker::Internet.user_name, email: Faker::Internet.email, password: 'test1234')}
+
+
+10.times do
+  Survey.create(creator_id: User.all.sample(1)[0].id, 
+    category: Faker::Lorem.sentence(3),
+    title: Faker::Lorem.sentence(5) )
+end
+
+20.times do
+  Question.create(survey_id: Survey.all.sample(1)[0].id,
+    content: Faker::Lorem.sentence(12))
+end
+
+30.times do
+  Answer.create(question_id: Question.all.sample(1)[0].id,
+    content: Faker::Lorem.sentence(12))
+end
+
+70.times do
+  Response.create(answer_id: Answer.all.sample(1)[0].id,
+    user_id: User.all.sample(1)[0].id)
+end
